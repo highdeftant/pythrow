5e8d83c09b64e05fd8d66e5152259c5843154526
highdeftant <a.allen.com@gmail.com>
2025-11-01 17:10:59 -0400

    changes

--- removeraw.py
+++ removeraw.py
@@ -1,21 +1,48 @@
 #!/bin/env python3
 
 import os
+import sys
+import shututil
 
-folder = "/home/gh0st/Documents"
-#folder = "/Volumes/LaCie"
+search_term = sys.argv[2]
+folder = "/Volumes/LaCie"
+raw_count = 0
 
-def checkraw(directory, term):
-    for file in os.listdir(directory):
-         if os.path.isfile(file):
-             if term in file:
-                 try:
-                     os.remove(filepath)
-                     print("Removed: {filepath}")
-                 except OSError as e:
-                     print("Error Removing {filepath}: {e}")
+
+def removeraw():
+    for path, dirname, filename in os.walk(folder, topdown=True):
+        for dirname in dirnames:
+            if "raw" in dirname.lower():
+                full_path = os.path.join(dirpath, dirname)
+                print(f"Found: {full_path}")
+
+
+def check_dir(folders):
+    folders = []
+    for item in os.listdir(directory):
+        try:
+            if os.path.isdir(item):
+                folders.append(item)
+        except OSError as e:
+            print("Error listing folder: {e}")
+
+def checkraw(folders, term):
+            for file in folders:
+             if os.path.isfile(file):
+                  if term in file:
+                      try:
+                          print("Removing RAW: {file}")
+                          os.remove(filepath)
+                          print("Removed: {file}")
+                          raw_count += 1
+                      except OSError as e:
+                          print("Error Removing {file}: {e}")
+
+
+
+def move_file(file):
+    pass
+
 
 if __name__ == "__main__":
-
-    print("Searching for RAW files....")
-    checkraw(folder, "raw")
+    removeraw()
