9c62dcab0222c5cf3be8460b78dbb212fe28b111
highdeftant <a.allen.com@gmail.com>
2025-11-01 17:26:06 -0400

    changes

--- removeraw.py
+++ removeraw.py
@@ -10,38 +10,13 @@ raw_count = 0
 
 
 def removeraw():
-    for path, dirname, filename in os.walk(folder, topdown=True):
-        for dirname in dirnames:
-            if "raw" in dirname.lower():
-                full_path = os.path.join(dirpath, dirname)
-                print(f"Found: {full_path}")
-
-
-def check_dir(folders):
-    folders = []
-    for item in os.listdir(directory):
-        try:
-            if os.path.isdir(item):
-                folders.append(item)
-        except OSError as e:
-            print("Error listing folder: {e}")
-
-def checkraw(folders, term):
-            for file in folders:
-             if os.path.isfile(file):
-                  if term in file:
-                      try:
-                          print("Removing RAW: {file}")
-                          os.remove(filepath)
-                          print("Removed: {file}")
-                          raw_count += 1
-                      except OSError as e:
-                          print("Error Removing {file}: {e}")
-
-
-
-def move_file(file):
-    pass
+    for path, dirs, file in os.walk(folder, topdown=True):
+        for dirname in dirs:
+            if sys.argv[1] in dirname.lower():
+                fullpath = os.path.join(path, dirname)
+                print("Found: {full_path}")
+                print("Removing {}...", fullpath)
+                shututil.rmtree(fullpath)
 
 
 if __name__ == "__main__":
